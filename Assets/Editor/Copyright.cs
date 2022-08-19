b8f993d15299cab372bb79acad65f482c4474ff1
LAPTOP-ATMCSSFC\瑞 <846183785@qq.com>
2022-08-19 17:23:55 +0800

    添加工具

---
+++ Assets/Editor/Copyright.cs
@@ -0,0 +1,34 @@
+﻿//=====================================================
+// - FileName:      #SCRIPTNAME#.cs
+// - Created:       #AuthorName#
+// - UserName:      #CreateTime#
+// - Email:         #AuthorEmail#
+// - Description:   
+// -  (C) Copyright 2008 - 2015, hehehuyu,Inc.
+// -  All Rights Reserved.
+//======================================================
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Copyright : UnityEditor.AssetModificationProcessor
+{
+    private const string AuthorName = "yangrui";
+    private const string AuthorEmail = "846183785@qq.com";
+
+    private const string DateFormat = "yyyy/MM/dd HH:mm:ss";
+    private static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs"))
+        {
+            string allText = File.ReadAllText(path);
+            allText = allText.Replace("#AuthorName#", AuthorName);
+            allText = allText.Replace("#AuthorEmail#", AuthorEmail);
+            allText = allText.Replace("#CreateTime#", System.DateTime.Now.ToString(DateFormat));
+            File.WriteAllText(path, allText);
+            UnityEditor.AssetDatabase.Refresh();
+        }
+
+    }
+}
---
+++ Assets/Editor/CreateFolder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+using System.IO;
+public class CreateFile : Editor
+{
+    /// <summary>
+    /// 创建常用文件夹
+    /// </summary>
+    [MenuItem("Tools/CreateFolder")]
+    public static void CreateFolder()
+    {
+        //I/0方法 全路径直接创建文件夹
+        string path = Application.dataPath + "/";
+        Directory.CreateDirectory(path + "Resources");
+        Directory.CreateDirectory(path + "Plugins");
+        Directory.CreateDirectory(path + "StreamingAssets");
+        Directory.CreateDirectory(path + "Editor");
+        Directory.CreateDirectory(path + "Scenes");
+        Directory.CreateDirectory(path + "Scripts");
+        Directory.CreateDirectory(path + "Scripts/CommonTool");
+        Directory.CreateDirectory(path + "Scripts/Date");
+        Directory.CreateDirectory(path + "Scripts/Globalinstance");
+        Directory.CreateDirectory(path + "Scripts/Myui");
+        Directory.CreateDirectory(path + "Models");
+        Directory.CreateDirectory(path + "UiImage");
+        Directory.CreateDirectory(path + "Materials");
+        AssetDatabase.Refresh();
+
+        //untiy 的方法
+        //if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+        //{
+        //    AssetDatabase.CreateFolder("Assets", "Resources");
+        //}
+        //AssetDatabase.CreateFolder("Assets", "Plugins");
+        //AssetDatabase.CreateFolder("Assets", "StreamingAssets");
+        //AssetDatabase.CreateFolder("Assets", "Editor");
+        //AssetDatabase.CreateFolder("Assets", "Scripts");
+
+    }
+}
